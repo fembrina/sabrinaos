1e67315341b3b9280634653296543c5fc00f8dc6
fembrina <72325712+fembrina@users.noreply.github.com>
2026-01-05 23:43:08 -0600

    Add copyright and license information to boot.s

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -1,6 +1,7 @@
 ; boot.s - The Entry Point for SabrinaOS
+; Copyright (C) 2026 fembrina
+; License: GPLv2
 
-; --- 1. Multiboot Header ---
 MAGIC    equ 0x1BADB002
 FLAGS    equ (1 << 0) | (1 << 1)
 CHECKSUM equ -(MAGIC + FLAGS)
@@ -11,26 +12,20 @@ align 4
     dd FLAGS
     dd CHECKSUM
 
-; --- 2. Stack Reservation ---
 section .bss
 align 16
 stack_bottom:
     resb 16384 ; 16KB
 stack_top:
 
-; --- 3. Entry Point ---
 section .text
 global _start
 extern kmain
 
 _start:
-    ; Set up the stack
     mov esp, stack_top
-
-    ; Enter the Kernel
     call kmain
 
-    ; Safety Hang (if kmain returns)
     cli
 .hang:
     hlt
