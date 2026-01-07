836514b1d53d2752626e438c08deac3c80b0056a
fembrina <72325712+fembrina@users.noreply.github.com>
2026-01-06 22:51:45 -0600

    License notice update (#4)
    * License Notice Update

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -1,7 +1,16 @@
 ; boot.s - The Entry Point for SabrinaOS
 ; Copyright (C) 2026 fembrina
-; License: GPLv2
-
+;
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License
+; as published by the Free Software Foundation; either version 2
+; of the License, or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+ 
 MAGIC    equ 0x1BADB002
 FLAGS    equ (1 << 0) | (1 << 1)
 CHECKSUM equ -(MAGIC + FLAGS)
